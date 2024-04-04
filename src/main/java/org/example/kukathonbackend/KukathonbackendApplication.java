d6c34dfcce5a6a9449cb30f98975227391d77039
bbbang105 <2018111366@dgu.ac.kr>
2024-04-04 13:41:57 +0900

    Init : 프로젝트 생성

---
+++ src/main/java/org/example/kukathonbackend/KukathonbackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.kukathonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukathonbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KukathonbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kukathonbackend/KukathonbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kukathonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukathonbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
