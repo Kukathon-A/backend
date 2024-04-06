376b629e9fdbb72dee9c8bb29604ab277ca3af52
bbbang105 <2018111366@dgu.ac.kr>
2024-04-06 23:00:36 +0900

    Feat : Route 엔터티 추가

---
+++ src/main/java/org/example/kukathonbackend/domain/route/domain/Route.java
@@ -0,0 +1,42 @@
+package org.example.kukathonbackend.domain.route.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.kukathonbackend.domain.user.domain.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Data
+@Table(name = "route")
+public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "route_id")
+    private Long id;
+
+    @Column(name = "start_location")
+    private String startLocation;
+
+    @Column(name = "arrive_location")
+    private String arriveLocation;
+
+    @Column(name = "start_latitude")
+    private String startLatitude;
+
+    @Column(name = "start_longitude")
+    private String startLongitude;
+
+    @Column(name = "arrive_latitude")
+    private String arriveLatitude;
+
+    @Column(name = "arrive_longitude")
+    private String arriveLongitude;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
