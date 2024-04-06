397acc0c1d25a6cdd4591c66f86f20c164630a58
bbbang105 <2018111366@dgu.ac.kr>
2024-04-07 05:33:20 +0900

    Refactor : 타입 수정

--- src/main/java/org/example/kukathonbackend/domain/timetable/dto/TimeTableInfo.java
+++ src/main/java/org/example/kukathonbackend/domain/timetable/dto/TimeTableInfo.java
@@ -3,13 +3,11 @@ package org.example.kukathonbackend.domain.timetable.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 @Builder
 public class TimeTableInfo {
-    LocalDateTime startWorkTime;
-    LocalDateTime startLunchTime;
-    LocalDateTime endLunchTime;
-    LocalDateTime endWorkTime;
+    String startWorkTime;
+    String startLunchTime;
+    String endLunchTime;
+    String endWorkTime;
 }
